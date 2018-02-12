1845c48f3f359b8e711744ee0997ec505cf11a95
DeeDeeQQ <34162801+DeeDeeQQ@users.noreply.github.com>
2018-02-12 16:29:26 +0200

    recursion delete and editing files

--- views/files/change.php
+++ views/files/change.php
@@ -9,7 +9,6 @@
 ?>
 <h3><?= $file ?></h3>
 <p><a href="<?= $back ?>">Go back</a></p>
-<?= var_dump($file) ?>
 <form action="<?= toUrl('/files/changeFile?path=' . urlencode($file)) ?>" method="post">
     <input type="hidden" name="file" value="<?= $file ?>">
     <textarea name="text" style="width: 85%;resize: both; overflow: auto"><?= $content ?></textarea>
